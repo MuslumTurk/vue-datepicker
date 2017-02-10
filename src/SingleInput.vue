8ff7d1cc880682f72e1a502575d1936382df710a
Mücahit Büyükyılmaz <mucahit@konya.edu.tr>
2017-02-10 18:13:45 +0300

    Initial Commit

---
+++ src/SingleInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <input class="input" :class="inputClass" type="text" :placeholder="placeholder" :readonly="readonly" v-model="value"/>
+</template>
+
+<script>
+export default {
+  props: {
+    alignment: String,
+    config: {
+      type: Object,
+      default: () => ({})
+    },
+    l10n: {
+      type: Object,
+      default: () => ({})
+    },
+    placeholder: {
+      type: String,
+      default: 'Pick date'
+    },
+    readonly: Boolean,
+    inputClass: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
